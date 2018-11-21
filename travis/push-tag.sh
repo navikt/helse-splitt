de55dd3cccf48ad0021e3cf4c9deb7bbf70ca126
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2018-11-21 09:38:28 +0100

    Remove Makefile and push naiserator.yaml to navikt/helse-iac

--- travis/push-tag.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ev
-
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
-    make release
-    git push --tags https://x-access-token:$INSTALLATION_TOKEN@github.com/navikt/helse-sykepengesoknadfilter.git HEAD:master
-fi
